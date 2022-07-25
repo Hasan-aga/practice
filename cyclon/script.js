f7e0c40c58439fd454d422e4f3b0884aae22f37d
Hasan-aga <careerforma@gmail.com>
2022-07-25 17:26:45 +0300

    change cursor when adding location

--- cyclon/script.js
+++ cyclon/script.js
@@ -1,7 +1,14 @@
 "use strict";
 
+const addButtonElement = document.querySelector(".content-input--add");
+const bodyElement = document.querySelector("body");
+
 let map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
+
+addButtonElement.addEventListener("click", function () {
+  bodyElement.classList.toggle("location-cursor");
+});
