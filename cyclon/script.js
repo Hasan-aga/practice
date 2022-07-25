2dd6dc121712f5a24e09f3008e0564b0e7271775
Hasan-aga <careerforma@gmail.com>
2022-07-25 17:26:45 +0300

    change cursor when adding location

--- cyclon/script.js
+++ cyclon/script.js
@@ -3,12 +3,27 @@
 const addButtonElement = document.querySelector(".content-input--add");
 const bodyElement = document.querySelector("body");
 
+let pickingLocationFlag = false;
+
 let map = L.map("map").setView([51.505, -0.09], 13);
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution: "© OpenStreetMap",
 }).addTo(map);
 
-addButtonElement.addEventListener("click", function () {
-  bodyElement.classList.toggle("location-cursor");
+bodyElement.addEventListener("click", function () {
+  if (pickingLocationFlag) {
+    console.log(pickingLocationFlag);
+    bodyElement.classList.remove("location-cursor");
+    pickingLocationFlag = !pickingLocationFlag;
+  }
 });
+
+addButtonElement.addEventListener("click", function () {
+  bodyElement.classList.add("location-cursor");
+  setTimeout(togglePickingLocationFlag, 300);
+});
+
+function togglePickingLocationFlag() {
+  pickingLocationFlag = !pickingLocationFlag;
+}
