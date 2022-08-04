ce045e6452269261f61706dcd9a1f583b96e797e
Hasan-aga <careerforma@gmail.com>
2022-08-04 16:12:08 +0300

    rename controller functions

--- cyclon/script.js
+++ cyclon/script.js
@@ -1,67 +1,64 @@
-import  view  from "./view.js";
+import view from "./view.js";
 import model from "./model.js";
-
-
 
 function controlMap() {
   view.createMap.call(view);
 }
 
-  function handleChangedCursor() {
-    console.log("listen to any click");
-    //reset on new clicks
-    view.listenToAnyClick();
+function controlChangedCursor() {
+  console.log("listen to any click");
+  //reset on new clicks
+  view.listenToAnyClick();
 
-    // listen to click on map only after add button is clicked
-    view.listenToMapClick(handleMapClick);
-  }
-
-  function handleGettingLocation() {
-    if (navigator.geolocation)
-      navigator.geolocation.getCurrentPosition(
-        view.centerMapOn.bind(view),
-        function () {
-          console.log("could not obtain location, displaying default map ");
-        }
-      );
-  }
-
-  function handleMapClick(event) {
-    const latlng = Object.values(event.latlng);
-    view.addMarker(latlng, view.map, "success");
-  }
-
-  const getJsonFromFetch = async function(name) {
-    const apiKey = "2693514c6f184c17ac5785c2da27facb";
-    const url = `https://api.geoapify.com/v1/geocode/search?text=${name}&format=json&apiKey=${apiKey}`;
-    console.log(url);
-    try {
-      const response = await fetch(url);
-      return await response.json();
-    } catch (e) {
-      throw e;
-    }
-  }
-
- const getLocationFromName= async function(countryName) {
-    try {
-      console.log(`fetching info for ${countryName}`);
-      const countryData = await getJsonFromFetch(countryName);
-      const latlng = [countryData.results[0].lat, countryData.results[0].lon];
-      console.log(latlng);
-      view.centerMapOn(latlng);
-      return await latlng;
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  // listen to click on map only after add button is clicked
+  view.listenToMapClick(controlMapClick);
 }
 
-app.view.createListItem("hello");
+function controlGettingLocation() {
+  if (navigator.geolocation)
+    navigator.geolocation.getCurrentPosition(
+      view.centerMapOn.bind(view),
+      function () {
+        console.log("could not obtain location, displaying default map ");
+      }
+    );
+}
+
+function controlMapClick(event) {
+  const latlng = Object.values(event.latlng);
+  view.addMarker(latlng, view.map, "success");
+}
+
+const getJsonFromFetch = async function (name) {
+  const apiKey = "2693514c6f184c17ac5785c2da27facb";
+  const url = `https://api.geoapify.com/v1/geocode/search?text=${name}&format=json&apiKey=${apiKey}`;
+  console.log(url);
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (e) {
+    throw e;
+  }
+};
+
+const getLocationFromName = async function (countryName) {
+  try {
+    console.log(`fetching info for ${countryName}`);
+    const countryData = await getJsonFromFetch(countryName);
+    const latlng = [countryData.results[0].lat, countryData.results[0].lon];
+    console.log(latlng);
+    view.centerMapOn(latlng);
+    return await latlng;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 function init() {
-  view.addOnLoadHandler()
-  view.listenToAddButton(handleChangedCursor);
-  view.listenToLocationButton(handleGettingLocation);
+  view.addOnLoadHandler(controlMap);
+  view.listenToAddButton(controlChangedCursor);
+  view.listenToLocationButton(controlGettingLocation);
   view.listenToLocationSearch(getLocationFromName);
 }
+
+init();
